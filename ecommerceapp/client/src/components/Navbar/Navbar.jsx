372bc6dae5306b5d93db1ae0b3e2cd05a3f06733
Smulanovich <santiagomulanovichsuarez@gmail.com>
2023-05-10 17:07:42 -0700

    I installed mui to have acces to multiple useful icons. I then modifed the Navbar.jsx file by finalizing its basic, structural layout and setting up all of the linking paths.

--- ecommerceapp/client/src/components/Navbar/Navbar.jsx
+++ ecommerceapp/client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ArrowDropDownCircleOutlinedIcon from '@mui/icons-material/ArrowDropDownCircleOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
+import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
 
- const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    );
-}
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+        <div className="wrapper">
+            <div className="left">
+                <div className="item">
+                    <Link to="/products/1">Candy Bars</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/2">Candy Corn</Link>
+                </div>
+                <div className="item">
+                    <Link to="/products/3">Candy Sticks</Link>
+                </div>
+            </div>
+            <div className="center">
+                <Link to="/">Candyland</Link>
+            </div>
+            <div className="right"></div>
+        </div>
+    </div>
+  );
+};
 
 export default Navbar;
