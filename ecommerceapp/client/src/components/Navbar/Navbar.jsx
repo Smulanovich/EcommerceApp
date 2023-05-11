ae6c6cf1896feb23448d8c428e4b34c3e942ee24
Smulanovich <santiagomulanovichsuarez@gmail.com>
2023-05-11 08:07:54 -0700

    I modified the Navbar.jsx file. The basic structure of the Navbar's layout is basically done, as well some of the basic routing attached to each link.

--- ecommerceapp/client/src/components/Navbar/Navbar.jsx
+++ ecommerceapp/client/src/components/Navbar/Navbar.jsx
@@ -5,27 +5,47 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import "./Navbar.css"; 
 
 
 const Navbar = () => {
   return (
     <div className="Navbar">
-        <div className="wrapper">
+        <div className="wrapper">   
             <div className="left">
                 <div className="item">
-                    <Link to="/products/1">Candy Bars</Link>
+                    <Link className="link" to="/products/CandyBars">Candy Bars</Link>
                 </div>
                 <div className="item">
-                    <Link to="/products/2">Candy Corn</Link>
+                    <Link className="link" to="/products/CandyCorn">Candy Corn</Link>
                 </div>
                 <div className="item">
-                    <Link to="/products/3">Candy Sticks</Link>
+                    <Link className="link" to="/products/CandySticks">Candy Sticks</Link>
                 </div>
             </div>
             <div className="center">
-                <Link to="/">Candyland</Link>
+                <Link className="link" to="/">Candyland</Link>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="item">
+                    <Link className="link" to="/">Homepage</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to="/">About</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to="/">Contact</Link>
+                </div>
+                <div className="icons">
+                    <SearchOutlinedIcon/>
+                    <AccountCircleOutlinedIcon/>
+                    <GradeOutlinedIcon/>
+                    <div className="cartIcon">
+                        <LocalMallOutlinedIcon/>
+                        <span>0</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   );
