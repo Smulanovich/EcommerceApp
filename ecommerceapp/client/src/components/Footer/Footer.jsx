d829ce5583f0697745e5a19e4ddc5158a236a432
Smulanovich <santiagomulanovichsuarez@gmail.com>
2023-05-14 13:04:58 -0700

    I finished the basic layout of the Footer.

--- ecommerceapp/client/src/components/Footer/Footer.jsx
+++ ecommerceapp/client/src/components/Footer/Footer.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import logo from "./logo.png"
+import './Footer.css'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="Footer">
+                <div className="item">
+                    <h1>Categories</h1>
+                    <div className="links">
+                    <Link className="link" to="/products/CandyBars">Candy Bars</Link>
+                    <Link className="link" to="/products/CandyCorn">Candy Corn</Link>
+                    <Link className="link" to="/products/CandySticks">Candy Sticks</Link>
+                    <Link className="link" to="/products/ExoticCandy">Exotic Candy</Link>
+                    <Link className="link" to="/products/PremiumCandy">Premium Candy</Link>
+                </div>
+                </div>
+                <img src={logo} alt="Website Logo" className="logo"/>
+        </div>
     );
 }
 
