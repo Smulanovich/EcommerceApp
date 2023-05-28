2f8ec4005f3dc46d4ab08ae03c8ca7c45c8fa573
Mario Prado <marioprado@s-149-142-27-109.resnet.ucla.edu>
2023-05-28 15:35:31 -0700

    Corrects display for register page.
    Instead of Saying "login" it now says register since this is the regsiter page.

--- ecommerceapp/client/src/pages/SignIn/Register.jsx
+++ ecommerceapp/client/src/pages/SignIn/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@email" id="email" name="email"/>
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password"/>
-                <button type="submit">Log In</button>
+                <button type="submit">Register</button>
             </form>
             <button className="link-btn" onClick={goToLogin}>Already have an account?Login here.</button>
         </div>
