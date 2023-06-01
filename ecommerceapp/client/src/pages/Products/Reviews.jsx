4a6b09a3507b30b6d4b3766d6a66eb19e8580d35
Mario Prado <marioprado@s-149-142-27-218.resnet.ucla.edu>
2023-05-31 22:04:18 -0700

    Adds a rudimentary Reviews.jsx file that will dispaly reviews if any for that specific product.

--- ecommerceapp/client/src/pages/Products/Reviews.jsx
+++ ecommerceapp/client/src/pages/Products/Reviews.jsx
@@ -1,38 +1,46 @@
-// import React, { useEffect, useState, useContext } from "react";
-// import axios from "axios";
-// import { CartContext } from "../Cart/CartProvider.jsx";
-// import { useLocation, useParams, Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { CartContext } from "../Cart/CartProvider.jsx";
+import { useLocation, useParams, Link } from "react-router-dom";
 
-// const reviewDisplay = (review) => {
-//     return (
-//         <div className="review">
-//             <h2>{review.authorEmail}</h2>
-//             <p>{review.comment}</p>
-//         </div>
-//     )
-// }
+const reviewDisplay = (review) => {
+   return (
+        <div className="review">
+            <h2>{review.authorEmail}</h2>
+            <p>{review.comment}</p>
+        </div>
+    )
+}
 
-// function Reviews () {
-//     const { productType, product } = useParams();
-//     const [reviews, setReviews] = useState([]);
-//     const location = useLocation();
+function Reviews () {
+    const { productType, product } = useParams();
+    const [reviews, setReviews] = useState([]);
+    const location = useLocation();
 
-//     useEffect(() => {
-//         // Make the HTTP request to fetch the products
-//         axios
-//             .get(`http://localhost:4000/api/${productType}/${product}/reviews`)
-//             .then((response) => {
-//                 setReviews(response.data);
-//             })
-//             .catch((error) => {
-//                 console.error("Error retrieving reviews:", error);
-//             });
-//         }, [productType, product, location]);
-//     }
+    useEffect(() => {
+        // Make the HTTP request to fetch the products
+        axios
+            .get(`http://localhost:4000/api/${productType}/${product}/reviews`)
+            .then((response) => {
+                setReviews(response.data);
+            })
+            .catch((error) => {
+                console.error("Error retrieving reviews:", error);
+            });
+        }, [productType, product, location]);
 
-//     if (!reviews) {
-//         return <h1>No reviews for the pr</h1>
-//     }
+    if (!reviews) {
+        return (
+        <h1>No reviews for the product</h1>
+        );
+    }
 
-
-// }
+    return (
+        <>
+            {reviews.map((review) => (
+            <reviewDisplay review={review} />
+        ))}
+        </>
+    );
+}
+export default Reviews;
