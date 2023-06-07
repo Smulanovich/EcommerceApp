970f308ab20fe0731d1fc5e4e100a186bfe0444f
Christian Kattie <ckattie2003@g.ucla.edu>
2023-06-07 08:14:34 -0700

    Changed addToCart button appearance

--- ecommerceapp/client/src/pages/Products/CandyDisplay.jsx
+++ ecommerceapp/client/src/pages/Products/CandyDisplay.jsx
@@ -1,26 +1,18 @@
 import React, { useContext } from "react";
 import { CartContext } from "../Cart/CartProvider.jsx";
-import "./CandyDisplay.css";
+import "./CandyDisplay.css"; 
 
 const CandyDisplay = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
-  if (!product || !product.name) {
-    return <></>; 
-  }
-
-  const productNameUpperCase = product.name ? product.name.toUpperCase() : "";
-
   return (
     <div className="product-container">
       <div className="product">
-        <h2>{productNameUpperCase}</h2>
+        <h2>{product.name}</h2>
         <img src={product.image_address} alt={`${product.name} img`} />
         <p>Price: {product.price}</p>
-        <button className="addToCart" onClick={() => addToCart(product)}>
-          Add to cart
-        </button>
       </div>
+      <button className="addToCart" onClick={() => addToCart(product)}>Add to cart</button>
     </div>
   );
 };
